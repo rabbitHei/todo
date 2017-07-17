35e535bce1b65f18bb0474d64a81425b454c050a
pipixia <pipixia@ppx.com>
2017-07-17 17:27:16 +0800

    02-展示任务列表功能实现

--- js/app.js
+++ js/app.js
@@ -7,5 +7,19 @@
 
 	function TodoController($scope) {
 		// Your starting point. Enjoy the ride!
+
+		// vm ---> viewmodel 视图模型
+		var vm = $scope;
+
+		// 1 展示任务列表
+		var todoList = [
+			{ id: 1, name: '抽烟', isCompleted: false },
+			{ id: 2, name: '喝酒', isCompleted: true },
+			{ id: 3, name: '烫头发', isCompleted: false }
+		];
+
+		vm.todoList = todoList;
+
+
 	}
 })(angular);
