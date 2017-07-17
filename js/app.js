cdfd6e67696f73bbbd30d42ac3a1d56c62390206
pipixia <pipixia@ppx.com>
2017-07-17 17:16:53 +0800

    01-初始化项目结构

---
+++ js/app.js
@@ -0,0 +1,11 @@
+(function (angular) {
+	'use strict';
+
+	angular
+		.module('todoApp', [])
+		.controller('TodoController', ['$scope', TodoController]);
+
+	function TodoController($scope) {
+		// Your starting point. Enjoy the ride!
+	}
+})(angular);
